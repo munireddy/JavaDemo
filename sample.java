2161b07bab9172d4a6f401d598611ceadeaf997b
munireddy <munisekhar.reddy@gmail.com>
2020-12-01 21:12:50 -0500

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,15 @@
+class  MyPrg1{
+ public static void main(String args[]){
+     int a = 100;
+     System.out.println("Hello World from a git Hub 1........ 0322 Jenkins Trigger");
+     System.out.println("Hello World from a git Hub 1........"+a);
+     System.out.println("Hello World from a git Hub 2");
+     System.out.println("12 New change added to the Java program");
+     System.out.println("Change added to trigger a SCM polled job");
+     System.out.println("Change added to trigger jenkins job 03224");
+     System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! New Line 0520  !!!!!!!!!!!!!!!!!!!!!!!!1");
+     System.out.println("##################################### New Line 0520  ########################1");
+     System.out.println("##################################### New Line 1201  ########################1");
+ }
+}
+// 
